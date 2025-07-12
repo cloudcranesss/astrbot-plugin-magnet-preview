27c0d44aa920b17fa0f65aff9b3ef48927c66fee
master <amy12138@yeah.net>
2025-07-12 09:14:28 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	README.md

--- froward_message.py
+++ froward_message.py
@@ -7,7 +7,7 @@ from astrbot.core import logger
 class ForwardMessage:
     def __init__(self, event: AstrMessageEvent, messages: list[str], screenshots: Optional[list] = None) -> None:
         self.event = event
-        self.platform = self.event.platform
+        self.platform = self.event.get_platform_name()
         logger.info(f"{self.platform} forward message: {messages}")
         self.self_id = int(self.event.get_self_id())
         self.messages = messages
@@ -18,25 +18,30 @@ class ForwardMessage:
         bot_name = "CloudCrane Bot"
         nodes = []
 
-        for message in self.messages:
-            # 处理消息
-            text_content = str(message) if message is not None else ""
-            nodes.append(
-                comp.Node(
-                    uin=uin,
-                    name=bot_name,
-                    content=[comp.Plain(text_content)]
+        if self.platform == "aiocqhttp":
+            for message in self.messages:
+                # 处理消息
+                text_content = str(message) if message is not None else ""
+                nodes.append(
+                    comp.Node(
+                        uin=uin,
+                        name=bot_name,
+                        content=[comp.Plain(text_content)]
+                    )
                 )
-            )
-        for screenshot in self.screenshots:
-            nodes.append(
-                comp.Node(
-                    uin=uin,
-                    name=bot_name,
-                    content=[comp.Image.fromURL(screenshot)]
+            for screenshot in self.screenshots:
+                nodes.append(
+                    comp.Node(
+                        uin=uin,
+                        name=bot_name,
+                        content=[comp.Image.fromURL(screenshot)]
+                    )
                 )
-            )
-
+        else:
+            message = "\r".join(self.messages)
+            yield self.event.plain_result(f"{message}")
+            for image in self.screenshots:
+                yield self.event.image_result(image)
         # 记录日志（复用预存变量）
         if self.screenshots:
             logger.info(f"{uin} forward screenshots: {self.screenshots}")
